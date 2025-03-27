837277b74371ee4204c05d0c83e6443eeb63c3d6
lomo <18810722011@163.com>
2025-03-27 18:36:22 +0800

    Commit

--- Source/LomoLib/Public/RapidUI/PropertyEditor/RapidPropertyEditor.h
+++ Source/LomoLib/Public/RapidUI/PropertyEditor/RapidPropertyEditor.h
@@ -10,6 +10,8 @@
 class UScrollBox;
 class UVerticalBox;
 class UButton;
+class UComboBoxString;
+class UUserWidget;
 
 /**
  * 运行时属性编辑器，类似于编辑器中的DetailsView
@@ -54,13 +56,29 @@ public:
 	/**
 	 * 属性改变时触发的委托
 	 */
-	// DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnPropertyChangedDelegate, UObject*, Object, FName, PropertyName, const FProperty*, Property);
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnPropertyChangedDelegate, UObject*, Object, FName, PropertyName, const FProperty*, Property);
 	
 	/**
 	 * 当任何属性改变时触发
 	 */
-	// UPROPERTY(BlueprintAssignable, Category = "Property Editor")
-	// FOnPropertyChangedDelegate OnPropertyChanged;
+	UPROPERTY(BlueprintAssignable, Category = "Property Editor")
+	FOnPropertyChangedDelegate OnPropertyChanged;
+
+	/**
+	 * 设置控件模板类
+	 * @param InIntSpinBoxClass Int类型SpinBox的模板类
+	 * @param InFloatSpinBoxClass Float类型SpinBox的模板类
+	 * @param InTextBoxClass 文本框的模板类
+	 * @param InCheckBoxClass 复选框的模板类
+	 * @param InComboBoxClass 下拉框的模板类（枚举类型使用）
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Property Editor")
+	void SetWidgetTemplates(TSubclassOf<UUserWidget> InLabelClass, 
+						   TSubclassOf<USpinBox> InFloatSpinBoxClass,
+						   TSubclassOf<USpinBox> InIntSpinBoxClass,
+						   TSubclassOf<UEditableTextBox> InTextBoxClass,
+						   TSubclassOf<UCheckBox> InCheckBoxClass,
+						   TSubclassOf<UComboBoxString> InComboBoxClass);
 
 protected:
 	/** 内容容器 */
@@ -75,6 +93,12 @@ protected:
 	
 	/** 创建基本类型属性控件 (float, int, FString等) */
 	UWidget* CreateBasicPropertyWidget(FProperty* Property, void* PropertyValue);
+
+	/** 创建枚举类型属性控件 */
+	UWidget* CreateEnumPropertyWidget(FEnumProperty* EnumProperty, void* PropertyValue);
+
+	/** 创建枚举类型属性控件(旧版本UE兼容) */
+	UWidget* CreateBytePropertyWidget(FByteProperty* ByteProperty, void* PropertyValue);
 
 	/** 创建TArray类型属性控件 */
 	UWidget* CreateArrayPropertyWidget(FArrayProperty* ArrayProperty, void* PropertyValue);
@@ -104,6 +128,10 @@ protected:
 	UFUNCTION()
 	void HandleBoolValueChanged(bool NewValue);
 
+	/** 处理枚举值变化 */
+	UFUNCTION()
+	void HandleEnumValueChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+
 private:
 	/** 当前编辑的对象 */
 	UPROPERTY(Transient)
@@ -111,6 +139,34 @@ private:
 
 	/** 存储控件与属性名称的映射关系 */
 	TMap<UWidget*, FName> WidgetToPropertyMap;
+
+	/** 模板类：文本标签 */
+	UPROPERTY(Transient)
+	TSubclassOf<UUserWidget> LabelTemplateClass;
+	
+	/** 模板类：float值的SpinBox */
+	UPROPERTY(Transient)
+	TSubclassOf<USpinBox> FloatSpinBoxTemplateClass;
+
+	/** 模板类：int值的SpinBox */
+	UPROPERTY(Transient)
+	TSubclassOf<USpinBox> IntSpinBoxTemplateClass;
+
+	/** 模板类：文本框 */
+	UPROPERTY(Transient)
+	TSubclassOf<UEditableTextBox> TextBoxTemplateClass;
+
+	/** 模板类：复选框 */
+	UPROPERTY(Transient)
+	TSubclassOf<UCheckBox> CheckBoxTemplateClass;
+
+	/** 模板类：下拉框（枚举用） */
+	UPROPERTY(Transient)
+	TSubclassOf<UComboBoxString> ComboBoxTemplateClass;
+
+	/** 创建或使用模板创建控件 */
+	template<class WidgetClass>
+	WidgetClass* CreateWidgetFromTemplate(TSubclassOf<WidgetClass> TemplateClass);
 
 	/** 获取属性显示名称 */
 	FString GetPropertyDisplayName(FProperty* Property) const;
